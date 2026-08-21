5add865b55c579f11f0ef4adc2b9afb9182cfab2
santflores227-ux <santflores227@gmail.com>
2026-08-20 21:59:32 -0600

    primeros documentos

---
+++ Codigo/Material.java
@@ -0,0 +1,23 @@
+public class Material {
+    private String nombre;
+    private double precio;
+
+    public Material(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+    public void actualizarPrecio(double nuevoPrecio){
+        this.precio = nuevoPrecio;
+    }
+    public pagoCosto crearCosto(){
+        return new pagoCosto();
+    }
+}
---
+++ Codigo/pagoCosto.java
@@ -0,0 +1,21 @@
+public class pagoCosto{
+    String descripcion;
+    int costo;
+    int dia;
+    boolean remarcado;
+    public pagoCosto(String descripcion, int costo, int dia){
+        if (descripcion == null){
+            this.descripcion = "Sin descripcion";
+        } else {
+            this.descripcion = descripcion;
+        }
+        this.costo = costo;
+        this.dia = dia;
+    }
+    public void setRemarcado(boolean remarcado){
+        this.remarcado = remarcado;
+    }
+    public String getDescripcion(){
+        return descripcion;
+    }
+}
