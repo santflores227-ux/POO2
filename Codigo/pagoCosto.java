29dc2f719de7786ee7f7b4811a0da25f9482fb84
santflores227-ux <santflores227@gmail.com>
2026-08-20 22:41:06 -0600

    fix code

--- Codigo/pagoCosto.java
+++ Codigo/pagoCosto.java
@@ -2,15 +2,15 @@ package Codigo;
 
 public class pagoCosto{
     String descripcion;
-    int costo;
+    double costo;
     int dia;
     boolean remarcado;
-    public pagoCosto(int costo,String descripcion,int dia){
+    public pagoCosto(double costo,String descripcion,int dia){
         this.descripcion = descripcion;
         this.costo = costo;
         this.dia = dia;
     }
-    public pagoCosto(int costo){
+    public pagoCosto(double costo){
         this.costo = costo;
     }
     public void setRemarcado(boolean remarcado){
