e03a1889d02a690247aa0214ad2fe7c0d52bfeb7
Santiago <santflores227@gmail.com>
2026-08-21 12:00:18 -0600

    cambio

--- Codigo/InterfazGrafica.java
+++ Codigo/InterfazGrafica.java
@@ -1,3 +1,10 @@
+
 public class InterfazGrafica {
-    
+    public void mostrarMenu() {
+        System.out.println("Bienvenido al sistema de gestión de materiales y pagos.");
+        System.out.println("Seleccione una opción:");
+        System.out.println("1. Crear material");
+        System.out.println("2. Crear pago de costo");
+        System.out.println("3. Salir");
+    }
 }
